c544331c58265be5b9023e420b6835e00bd8d953
Jan Cichosz <jan.cichosz@solwena.com>
2022-03-03 21:07:28 +0100

    mongoose model with statics and typescript - alfa

--- src/models/commands.ts
+++ src/models/commands.ts
@@ -1,19 +1,45 @@
-import mongoose from "mongoose";
+import { Schema, model, Model } from "mongoose";
 import Ajv, { JSONSchemaType } from "ajv";
 
 const ajv = new Ajv();
 
 // 1. Create an interface representing a document in MongoDB.
-export interface command {
+export interface ICommand {
 	userId: string;
 	command: string;
-	description: string;
-	timestamp: string;
+	description?: string;
+	timestamp?: string;
 }
+
+export interface ICommandBaseDocument extends ICommand, Document {
+	findLastCommandFromUser(this: Model<ICommandBaseDocument>, id: string): Promise<ICommandBaseDocument>;
+}
+
+export interface ICommandBaseModel extends Model<ICommandBaseDocument> {
+	findLastCommandFromUser(this: Model<ICommandBaseDocument>, id: string): Promise<ICommandBaseDocument>;
+}
+
+const commandSchema = new Schema<ICommandBaseDocument, ICommandBaseModel>(
+	{
+		userId: { type: String, required: true },
+		command: { type: String, required: true },
+		description: { type: String, required: false },
+		timestamp: { type: String, required: false },
+	},
+	{ timestamps: true, _id: false }
+);
+
+commandSchema.statics.findLastCommandFromUser = async function (this: Model<ICommandBaseDocument>, id: string) {
+	console.log("asdasda JC@22");
+	return this.findOne({ id });
+};
+
+// 5. Create a Model - MongoDB
+export const commandModel = model<ICommandBaseDocument, ICommandBaseModel>("Commands", commandSchema);
 
 // 2. Create a validation schema - AJV.
 
-const validationSchema: JSONSchemaType<command> = {
+const validationSchema: JSONSchemaType<ICommand> = {
 	type: "object",
 	properties: {
 		userId: { type: "string" },
@@ -27,20 +53,4 @@ const validationSchema: JSONSchemaType<command> = {
 
 // 3. Create validation function - AJV
 
-export const isCommand = ajv.compile(validationSchema);
-
-// 4. Create a Schema corresponding to the document interfac - MongoDB.
-
-const modelSchema = new mongoose.Schema<command>(
-	{
-		userId: { type: String, required: true },
-		command: { type: String, required: true },
-		description: { type: String, required: false },
-		timestamp: { type: String, required: false },
-	},
-	{ timestamps: true, _id: false }
-);
-
-// 5. Create a Model - MongoDB
-
-export const commandModel = mongoose.model<command>("Commands", modelSchema);
+const isCommand = ajv.compile(validationSchema);
